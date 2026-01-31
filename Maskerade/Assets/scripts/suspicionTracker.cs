ff219b58d256a6f382c3a90fab3db7f4abc517be
Will Vanlint <william2.vanlint@live.uwe.ac.uk>
2026-01-31 12:55:59 +0000

    bug fix
    changed logic error and cleaned script name

--- Maskerade/Assets/scripts/suspicionTracker.cs
+++ Maskerade/Assets/scripts/suspicionTracker.cs
@@ -3,64 +3,73 @@ using UnityEngine;
 using UnityEngine.UI;
 public class suspicionTracker : MonoBehaviour
 {
-    [Header("Settings")]
-    [SerializeField] private Image bar;
+	[Header("Settings")]
+	[SerializeField] private Image bar;
 	[SerializeField] private int currentSuspicion = 5;
-    [SerializeField] private int maxSuspicion = 100;
+	[SerializeField] private int maxSuspicion = 100;
 
-    [Header("Animation")]
-    [SerializeField, Range(0, 0.5f)] private float animationTime = 0.25f;
-    private Coroutine _fillRoutine;
-    
-    private void Start()
-    {
-        UpdateBar();
-    }
-    private void UpdateBar()
-    {
-        if (maxSuspicion <= 0)
-        {
-            bar.fillAmount = 0;
-            return;
-        }
+	[Header("Animation")]
+	[SerializeField, Range(0, 0.5f)] private float animationTime = 0.25f;
+	private Coroutine _fillRoutine;
 
-        float fillAmount = (float) currentSuspicion / maxSuspicion; 
-        bar.fillAmount = fillAmount;
-    }
+	public void setCurrentSuspicion(int suspicion)
+	{
+		currentSuspicion = suspicion;
+	}
 
-    public bool ChangeSuspicionByAmount(int amount)
-    {
-        
-        currentSuspicion += amount;
-        currentSuspicion = Mathf.Clamp(currentSuspicion, 0, maxSuspicion);
+	private void Start()
+	{
+		UpdateBar();
+	}
+	private void UpdateBar()
+	{
+		if (maxSuspicion <= 0)
+		{
+			bar.fillAmount = 0;
+			return;
+		}
 
-        bar.fillAmount = (float)currentSuspicion / maxSuspicion;
-        return true;
-    }
+		float fillAmount = (float)currentSuspicion / maxSuspicion;
+		bar.fillAmount = fillAmount;
+	}
 
-    private void TriggerFillAnimation()
-    {
-        float targetFill = (float)currentSuspicion / maxSuspicion;
-        if (Mathf.Approximately(bar.fillAmount, targetFill))
-            return;
-        if (_fillRoutine != null)
-            StopCoroutine(_fillRoutine);
+	public bool ChangeSuspicionByAmount(int amount)
+	{
 
-        _fillRoutine = StartCoroutine(SmoothlyTransitionToNewValue(targetFill));
-    }
+		currentSuspicion += amount;
+		currentSuspicion = Mathf.Clamp(currentSuspicion, 0, maxSuspicion);
 
-    private IEnumerator SmoothlyTransitionToNewValue(float targetFill)
-    {
-        float originFill = bar.fillAmount;
-        float elapsedTime = 0.0f;
-        while (elapsedTime < animationTime)
-        {
-            elapsedTime += Time.deltaTime;
-            float time = elapsedTime / animationTime;
-            bar.fillAmount = Mathf.Lerp(originFill, targetFill, time);
-            yield return null;
-        }
-        bar.fillAmount = targetFill;
-    }
+		bar.fillAmount = (float)currentSuspicion / maxSuspicion;
+		if (currentSuspicion == maxSuspicion)
+		{
+			//game over
+		}
+		return true;
+	}
+
+	private void TriggerFillAnimation()
+	{
+		float targetFill = (float)currentSuspicion / maxSuspicion;
+		if (Mathf.Approximately(bar.fillAmount, targetFill))
+			return;
+		if (_fillRoutine != null)
+			StopCoroutine(_fillRoutine);
+
+		_fillRoutine = StartCoroutine(SmoothlyTransitionToNewValue(targetFill));
+	}
+
+	private IEnumerator SmoothlyTransitionToNewValue(float targetFill)
+	{
+		float originFill = bar.fillAmount;
+		float elapsedTime = 0.0f;
+		while (elapsedTime < animationTime)
+		{
+			elapsedTime += Time.deltaTime;
+			float time = elapsedTime / animationTime;
+			bar.fillAmount = Mathf.Lerp(originFill, targetFill, time);
+			yield return null;
+		}
+		bar.fillAmount = targetFill;
+	}
 }
 
